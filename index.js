1449ac138462aa64ece32661426934567803a8aa
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-21 00:35:43 +0530

    Run the server on localhost

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+// create a root route 
+app.get('/',(req, res)=>{
+    res.send('Tech cosmos server is running...');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on PORT:${port}`);
+})
