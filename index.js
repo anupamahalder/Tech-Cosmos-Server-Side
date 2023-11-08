27cc012600e4fd64e825bf76ca431dcde02d5824
Anupama_Halder <anupamahalder2022@gmail.com>
2023-11-08 23:39:03 +0530

    Added patch method to headers

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://654bc9ff6091fc2327a62181--idyllic-parfait-a7e9f6.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
